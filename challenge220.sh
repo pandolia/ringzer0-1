82d769716572604351ea3bde5f328edb131d282c
James Allardice <james.allardice@fr.clara.net>
2016-03-17 18:50:34 +0100

    with flag

--- challenge220.sh
+++ challenge220.sh
@@ -1,5 +1,13 @@
 on level3 do:
-/?in/m??ir /tmp/jal4/;cp flag.txt /tmp/jal4/;/?in/chmo? -R 777 /tmp/jal4
+/?in/m??ir /tmp/jal4/;cp flag.txt /tmp/temp/;/?in/chmo? -R 777 /tmp/temp/
+
+The question marks mean we can get around the check_spacecheck_space limits
 
 Then reconnect to level1 and get a bash shell:
-cat /tmp/jal4/flag.txt >&2
+cat /tmp/temp/flag.txt >&2
+
+
+
+
+
+FLAG-s9wXyc9WKx1X6N9G68fCR0M78sx09D3j

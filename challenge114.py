631c2f4c0d9db2d2e045fcd8f9f10f09f98e6b7f
James Allardice <james.allardice@fr.clara.net>
2016-03-25 10:35:29 +0100

    challenge 114

---
+++ challenge114.py
@@ -0,0 +1,16 @@
+MSG = "VoiCI unE SUpeRbe reCeTtE cONcontee pAR un GrouPe d'ArtistEs culinaiRe, dONT le BOn Gout et lE SeNs de LA cLasSe n'est limIteE qUe par LE nombre DE cAlOries qU'ils PeUVEnt Ingurgiter. Ces virtuoses de la friteuse vous presente ce petit clip plein de gout savoureux !!VoiCI unE SUpeRbe reCeTtE cONcontee pAR un GrouPe d'ArtistEs culinaiRe, dONT le BOn Gout et lE SeNs de LA cLasSe n'est limIteE qUe par LE nombre DE cAlOries qU'ils PeUVEnt Ingurgiter. Ces virtuoses de la friteuse vous presente ce petit clip plein de gout savoureux !!"
+
+bacon = ''
+
+for i in range(len(MSG)):
+    if MSG[i].isupper():
+        bacon += 'B'
+    if MSG[i].islower():
+        bacon += 'A'
+
+print bacon
+
+"""
+run the result through the solver on this site:
+http://rumkin.com/tools/cipher/baconian.php
+"""

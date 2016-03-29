c421142d9211d3482db46000dd46c319606d7697
James Allardice <james.allardice@fr.clara.net>
2016-03-29 11:20:57 +0200

    challenge154

--- challenge66.py
+++ challenge66.py
@@ -8,7 +8,7 @@ DOESN'T WORK
 def bruteforce(charset, maxlength):
     return (''.join(candidate)
         for candidate in itertools.chain.from_iterable(itertools.product(charset, repeat=i)
-        for i in range(1, maxlength + 1)))
+        for i in range(3, maxlength + 1)))
 
 def netcat(hostname, port, content):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -29,12 +29,16 @@ if __name__ == '__main__':
     s.connect(("ringzer0team.com",60000))
     s.sendall("0")
     data = s.recv(9999)
-    for password in bruteforce('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',1):
-        print password
-        s.sendall("00000" + password)
+    for password in bruteforce('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',3):
+        #print password
+        s.sendall("03e" + password + "00000000000")
         data = s.recv(9999)
-
         print data
+        if float(data.split()[4]) > 0.000030:
+            print password
+
+
+        #print data
         if not "Wrong" in data:
             print data
             s.close()
